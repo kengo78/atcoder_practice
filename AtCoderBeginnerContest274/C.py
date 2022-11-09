e40038395c8541c99e146e24688908bbaae31bef
kengo78 <fulken35@gmail.com>
2022-11-09 13:20:09 +0900

    add:C.py

--- AtCoderBeginnerContest274/C.py
+++ AtCoderBeginnerContest274/C.py
@@ -1,15 +1,27 @@
 import sys
-import itertools
+# import collections
+from collections import defaultdict, Counter
 # Re
-def main(lines):
-    n = int(lines[0])
-    s_list = tuple(lines[1].split(' '))
-    tuple_list = sorted(list(itertools.permutations(s_list)))
-    K = tuple_list.index(s_list)
-    print(' '.join(tuple_list[K-1]))
+# def main(lines):
+#     n,m = map(int, lines[0].split(' '))
+#     # keys = [i for i in range(0, m)]
+#     result = defaultdict(int)
+#     for i,v in enumerate(lines[1:]):
+#         for j,k in enumerate(v):
+#             if k == '#':
+#                 result[j] += 1
+#             else:
+#                 result.setdefault(j, 0)
+#     result = sorted(result.items(), key=lambda i: i[0])
+#     result = [str(i[1]) for i in result]
+#     print(' '.join(result))
     
 if __name__ == '__main__':
-    lines = []
-    for l in sys.stdin:
-        lines.append(l.rstrip('\r\n'))
-    main(lines)
+    n = int(input())
+    m = list(map(int, input().split(' ')))
+    result = [0] * (2 * n +1)
+    for i,v in enumerate(m):
+        result[2*i+1]=result[v-1]+1
+        result[2*i+2]=result[v-1]+1
+        
+    print(*result,sep="\n")
