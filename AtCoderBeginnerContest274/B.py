70bdd2ed612e00cb88098426811bc1d74e0cb878
kengo78 <fulken35@gmail.com>
2022-11-09 13:20:01 +0900

    add:B.py

--- AtCoderBeginnerContest274/B.py
+++ AtCoderBeginnerContest274/B.py
@@ -1,20 +1,30 @@
-import sys
-def main(lines):
-    n= map(int, lines[0].split(' '))
-    lines = lines[1:]
-    record = {}
-    for i in range(m):
-        record.setdefault(str(i+1), [])
-    for i,v in enumerate(lines):
-        a,b = map(int, v.split(' '))
-        record[str(a)].append(str(b))
-        record[str(b)].append(str(a))
-    for i in record.keys():
-        record[i].sort()
-        # if record[i] == []:
-        print(str(len(record[i])),' '.join(record[i]))
+# import sys
+# # import collections
+# from collections import defaultdict, Counter
+# # Re
+# def main(lines):
+#     n,m = map(int, lines[0].split(' '))
+#     # keys = [i for i in range(0, m)]
+#     result = defaultdict(int)
+#     for i,v in enumerate(lines[1:]):
+#         for j,k in enumerate(v):
+#             if k == '#':
+#                 result[j] += 1
+#             else:
+#                 result.setdefault(j, 0)
+#     result = sorted(result.items(), key=lambda i: i[0])
+#     result = [str(i[1]) for i in result]
+#     print(' '.join(result))
+    
 if __name__ == '__main__':
-    lines = []
-    for l in sys.stdin:
-        lines.append(l.rstrip('\r\n'))
-    main(lines)
+    # lines = []
+    # for l in sys.stdin:
+    #     lines.append(l.rstrip('\r\n'))
+    # main(lines)
+    n, m = list(map(int, input().split( )))
+    grid = [input() for _ in range(n)]
+    # print('grid', grid, type(grid))
+    # print('*grid', *grid)
+    res = [''.join(col).count('#') for col in zip(*grid)]
+    # print('res', res)
+    print(' '.join(str(v) for v in res))
